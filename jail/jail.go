3c429d410097f9223fa5ad8eba416036226ac9b3
YEVHENII SHCHERBINA <yevhenii@coder.com>
2025-12-04 16:42:55 +0000

    refactor: linux jail

--- jail/jail.go
+++ jail/jail.go
@@ -2,7 +2,6 @@ package jail
 
 import (
 	"fmt"
-	"log"
 	"log/slog"
 	"os"
 	"os/exec"
@@ -131,53 +130,6 @@ func (l *LinuxJail) Close() error {
 	if err != nil {
 		l.logger.Error("Failed to clean up networking", "error", err)
 		// Continue with other cleanup even if this fails
-	}
-
-	return nil
-}
-
-type command struct {
-	description string
-	cmd         *exec.Cmd
-	ambientCaps []uintptr
-}
-
-type commandRunner struct {
-	commands []*command
-}
-
-func newCommandRunner(commands []*command) *commandRunner {
-	return &commandRunner{
-		commands: commands,
-	}
-}
-
-func (r *commandRunner) run() error {
-	for _, command := range r.commands {
-		command.cmd.SysProcAttr = &syscall.SysProcAttr{
-			AmbientCaps: command.ambientCaps,
-		}
-
-		output, err := command.cmd.CombinedOutput()
-		if err != nil {
-			return fmt.Errorf("failed to %s: %v, output: %s", command.description, err, output)
-		}
-	}
-
-	return nil
-}
-
-func (r *commandRunner) runIgnoreErrors() error {
-	for _, command := range r.commands {
-		command.cmd.SysProcAttr = &syscall.SysProcAttr{
-			AmbientCaps: command.ambientCaps,
-		}
-
-		output, err := command.cmd.CombinedOutput()
-		if err != nil {
-			log.Printf("failed to %s: %v, output: %s", command.description, err, output)
-			continue
-		}
 	}
 
 	return nil
--- jail/jail_test.go
+++
@@ -1,9 +0,0 @@
-package jail
-
-import "testing"
-
-// Stub test file - tests removed
-func TestStub(t *testing.T) {
-	// This is a stub test
-	t.Skip("stub test file")
-}
