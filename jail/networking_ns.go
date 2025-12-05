a25c9ccef267b85067555d2c10d6ca239ec73ec5
YEVHENII SHCHERBINA <yevhenii@coder.com>
2025-12-05 17:28:29 +0000

    remove commented out code

--- jail/networking_ns.go
+++ jail/networking_ns.go
@@ -10,17 +10,6 @@ import (
 // namespace. This runs inside the child process after it has been
 // created and moved to its own network namespace.
 func SetupChildNetworking(vethNetJail string) error {
-	//showCmd := exec.Command("ip", "link", "show")
-	//showCmd.SysProcAttr = &syscall.SysProcAttr{
-	//	AmbientCaps: []uintptr{uintptr(unix.CAP_NET_ADMIN)},
-	//}
-	//output, err := showCmd.CombinedOutput()
-	//if err != nil {
-	//	fmt.Printf("error (ip link show): %v\n", err)
-	//	return err
-	//}
-	//fmt.Printf("output (ip link show): %s\n", output)
-
 	runner := newCommandRunner([]*command{
 		{
 			"configure namespace veth",
