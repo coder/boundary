cb7c58d43c471403511499dca14c2d1c5653beb6
YEVHENII SHCHERBINA <yevhenii@coder.com>
2026-01-07 18:37:27 +0000

    test: add more e2e tests for http paths evaluation

--- e2e_tests/nsjail/http_path_test.go
+++ e2e_tests/nsjail/http_path_test.go
@@ -43,3 +43,57 @@ func TestHTTPPath(t *testing.T) {
 		nt.ExpectDeny("https://dev.coder.com/api/v3")
 	})
 }
+
+func TestHTTPPathWildCardSymbol(t *testing.T) {
+	// Create and configure nsjail test
+	nt := NewNSJailTest(t,
+		WithNSJailAllowedRule("domain=jsonplaceholder.typicode.com path=/todos/*"),
+		WithNSJailAllowedRule("domain=dev.coder.com path=/api/*"),
+		WithNSJailLogLevel("debug"),
+	).
+		Build().
+		Start()
+
+	// Ensure cleanup
+	defer nt.Stop()
+
+	// Test allowed HTTP request
+	t.Run("HTTPRequestThroughBoundary", func(t *testing.T) {
+		expectedResponse := `{
+  "userId": 1,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+}`
+		nt.ExpectAllowed("http://jsonplaceholder.typicode.com/todos/1", expectedResponse)
+	})
+
+	t.Run("HTTPRequestThroughBoundary", func(t *testing.T) {
+		expectedResponse := `{
+  "userId": 1,
+  "id": 2,
+  "title": "quis ut nam facilis et officia qui",
+  "completed": false
+}`
+		nt.ExpectAllowed("http://jsonplaceholder.typicode.com/todos/2", expectedResponse)
+	})
+
+	// Test allowed HTTPS request
+	t.Run("HTTPSRequestThroughBoundary", func(t *testing.T) {
+		expectedResponse := `{"message":"👋"}
+`
+		nt.ExpectAllowed("https://dev.coder.com/api/v2", expectedResponse)
+	})
+
+	// Test denied HTTP request
+	t.Run("HTTPRequestThroughBoundary", func(t *testing.T) {
+		nt.ExpectDeny("http://jsonplaceholder.typicode.com")
+		nt.ExpectDeny("http://jsonplaceholder.typicode.com/todos")
+	})
+
+	//// Test denied HTTPS request
+	t.Run("HTTPRequestThroughBoundary", func(t *testing.T) {
+		nt.ExpectDeny("https://dev.coder.com")
+		nt.ExpectDeny("https://dev.coder.com/api")
+	})
+}
