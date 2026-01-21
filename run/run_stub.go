e36b6e079e8e524afafb6be62d45aa0ee00d1def
Yevhenii Shcherbina <yevhenii@coder.com>
2026-01-21 16:31:54 +0000

    chore: allow compilation on non-linux platforms

---
+++ run/run_stub.go
@@ -0,0 +1,17 @@
+//go:build !linux
+
+package run
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"runtime"
+
+	"github.com/coder/boundary/config"
+)
+
+func Run(ctx context.Context, logger *slog.Logger, cfg config.AppConfig) error {
+	return fmt.Errorf("boundary is only supported on Linux, current platform: %s", runtime.GOOS)
+}
+
